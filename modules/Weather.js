72d96f194f7485c70ecf433a69ce00d6b2578592
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-02-13 18:28:18 +0100

    maj ressources

--- modules/Weather.js
+++ modules/Weather.js
@@ -2,9 +2,50 @@ class Weather {
   constructor() {
     this.logoWeather = document.querySelector(".weather-logo");
     this.logoWeather = [];
+    const temperatureScale = [
+      {
+        scale: [-20, -10],
+        quality: "glacial",
+        src: "https://fonts.gstatic.com/s/e/notoemoji/latest/1f976/lottie.json",
+      },
+      {
+        scale: [-9, 0],
+        quality: "tres froid",
+        src: "tres froid",
+      },
+      {
+        scale: [1, 10],
+        quality: "frais",
+        src: "frais",
+      },
+      {
+        scale: [11, 20],
+        quality: "doux",
+        src: "doux",
+      },
+      {
+        scale: [21, 25],
+        quality: "agreable",
+        src: "agreable",
+      },
+      {
+        scale: [26, 30],
+        quality: "chaud",
+        src: "chaud",
+      },
+      {
+        scale: [31, 35],
+        quality: "tres chaud",
+        src: "tres chaud",
+      },
+      {
+        scale: [36, 40],
+        quality: "caniculaire",
+        src: "caniculaire",
+      },
+    ];
 
     this.temperature = document.querySelector(".temperature-info");
-    this.uvIndex = document.querySelector(".uv-info");
 
     this.init();
   }
